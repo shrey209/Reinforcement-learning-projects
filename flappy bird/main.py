0eae924ca11415a57ad08bd4376b64b3d29dd4c2
shrey209 <shreyashkumar2022@gmail.com>
2025-06-05 11:11:53 +0530

    environement setup

--- flappy bird/main.py
+++ flappy bird/main.py
@@ -1,7 +1,23 @@
 
+import flappy_bird_gymnasium
+import gymnasium
 
 
 def main():
+    env = gymnasium.make("FlappyBird-v0", render_mode="human", use_lidar=True)
+
+    obs, _ = env.reset()
+    while True:
+    
+        action = env.action_space.sample()
+
+  
+        obs, reward, terminated, _, info = env.step(action)
+    
+        if terminated:
+            break
+
+    env.close()
     print("flappy-bird game starting...")
 
 
