a3c6d62c82db9aa74ab2a5f610e4908fe5f76d63
shrey209 <shreyashkumar2022@gmail.com>
2025-06-07 01:36:01 +0530

    adding optmizers

--- flappy-bird/main.py
+++ flappy-bird/main.py
@@ -1,7 +1,8 @@
 import flappy_bird_gymnasium
 import gymnasium
 from dqn import DQN
-import torch
+import torch 
+from torch import nn
 import itertools
 from replay_mem import ReplayMemory
 import numpy as np
@@ -16,13 +17,19 @@ class Agent:
         self.epsilon_init = 1.0
         self.epsilon_decay = 0.9995
         self.epsilon_min = 0.05
+        self.loss_function = torch.nn.MSELoss()
+        self.learning_rate = 0.001
+        self.optimizer = torch.optim.Adam(policy_dqn.parameters())
 
     def run(self, istraining=True, render=False):
         env = gymnasium.make("CartPole-v1", render_mode="human" if render else None)
         num_actions = env.action_space.n
         num_states = env.observation_space.shape[0]
         policy_dqn = DQN(num_states, num_actions).to(device)
+        target_dqn = DQN(num_states, num_actions).to(device)
+        target_dqn.load_state_dict(policy_dqn.state_dict())
 
+        step_count=0
         reward_per_episode = []
         epsilon_history = []
 
@@ -51,15 +58,42 @@ class Agent:
                 if istraining:
                     memory.append((state, action, new_state, reward, terminated))
 
+                step_count+=1
+
                 state = new_state
 
             reward_per_episode.append(episode_reward)
             epsilon = max(epsilon * self.epsilon_decay, self.epsilon_min)
             epsilon_history.append(epsilon)
 
+            if(len(memory) >= self.batch_size):
+                batch = memory.sample(self.batch_size)
+                self.optimize_model(batch,policy_dqn, target_dqn)
+
+                if step_count % 10 == 0:
+                    target_dqn.load_state_dict(policy_dqn.state_dict())
+                    step_count = 0
+            
+                
+
         env.close()
         print("flappy-bird game starting...")
 
+
+def optimize(self, batch, policy_dqn, target_dqn):
+    for state, action, new_state, reward, terminated in batch:
+        if terminated:
+            target_dqn_value = reward
+        else:
+            with torch.no_grad():
+                target_q=reward + self.discount_factor * torch.max(target_dqn(new_state)).max()
+
+        current=policy_dqn(state)
+        loss=self.loss_fn(current_q, target_q)
+
+        self.optimizer.zero_grad()
+        loss.backward()
+        self.optimizer.step()
 def main():
     agent = Agent()
     agent.run(istraining=True, render=True)
